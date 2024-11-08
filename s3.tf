abc20b3988ab5e0a6537896b003c69fdfc9b179c
pradeepkudukkil <88960052+pradeepkudukkil@users.noreply.github.com>
2024-11-08 12:29:04 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,9 @@
+# main.tf in GitHub
+provider "aws" {
+  region = "us-west-2"  # specify your AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-s3-bucket-name-pradeepk"  # specify a unique bucket name
+  acl    = "private"
+}
